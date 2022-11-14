82956214abbafc511ac7c77ff459e3dcdba5816a
Maximo Garcia <maximogarcia006@gmail.com>
2022-11-13 23:45:55 -0300

    make it better

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const app = require('../backend/src/app');
-require('./src/app');
-
-async function  main(){
-     await app.listen(app.get('port'))
-    console.log('server on port', app.get('port'))
-}
-main();
